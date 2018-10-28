a19a524d110e0019052cd16de6e94370f21ae46d
陈沛文 <charlieholy@github.com>
2018-10-28 10:37:41 +0800

    md

---
+++ caller.c
@@ -0,0 +1,12 @@
+int main(){
+	char buf[1024];
+	int a,b,res;
+	char* mystr = "calc..\n";
+	char* emsg = "Error in add\n";
+	a = 5;b = 10;
+	mywrite(1,mystr,strlen(mystr));
+//	if(myadd(a,b,&res)){
+//		sprintf(buf,"The result is %d\n",res);
+//		mywrite()
+//}
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 init
 as --32 -o callee.o callee.s
+yum install glibc-devel.i686 libstdc++-devel.i686
+gcc -m32 -o caller caller.c callee.o
