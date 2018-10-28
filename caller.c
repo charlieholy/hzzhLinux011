cdfc71f73cddd66f6d2d7315386369b61b33a9c2
陈沛文 <charlieholy@github.com>
2018-10-28 10:47:16 +0800

    md

--- caller.c
+++ caller.c
@@ -5,8 +5,11 @@ int main(){
 	char* emsg = "Error in add\n";
 	a = 5;b = 10;
 	mywrite(1,mystr,strlen(mystr));
-//	if(myadd(a,b,&res)){
-//		sprintf(buf,"The result is %d\n",res);
-//		mywrite()
-//}
+	if(myadd(a,b,&res)){
+		sprintf(buf,"The result is %d\n",res);
+		mywrite(1,buf,strlen(buf));}else{
+	mywrite(1,emsg,strlen(emsg));
+
+}
+return 0;
 }
